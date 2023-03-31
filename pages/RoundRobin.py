b04f307686a8674055c5833fbc1f5176429b7fee
Joseph Boley <107160857+Fireboltssbm@users.noreply.github.com>
2023-03-30 21:57:14 -0500

    Cleaned up equation

--- pages/RoundRobin.py
+++ pages/RoundRobin.py
@@ -10,7 +10,7 @@ average_time = st.number_input('Enter Average Time Per Set', 0)
 number_players = st.number_input('Enter Number of Players/Teams in Pool', 0)
 number_setups = st.number_input('Enter Number of Setups per pool', 1)
 
-total_time: float = (1.33*average_time*(number_players*number_players-1))/(2*number_setups)
+total_time = (1.33*average_time*(number_players*(number_players-1)))/(2*number_setups)
 
 
 st.header('total time needed for round robin pools')
