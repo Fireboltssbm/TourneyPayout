411d4770f2db737d31d6e76b2f44fe380e0e813a
Joseph Boley <107160857+Fireboltssbm@users.noreply.github.com>
2023-03-30 21:35:55 -0500

    fixed "divide by 0" error

--- Pages/RoundRobin.py
+++ Pages/RoundRobin.py
@@ -8,7 +8,7 @@ st.subheader('\n\n')
 efficiency_factor = st.number_input('Enter Effiency Factor', 0)
 average_time = st.number_input('Enter Average Time Per Set', 0)
 number_players = st.number_input('Enter Number of Players/Teams in Pool', 0)
-number_setups = st.number_input('Enter Number of Setups per pool', 0)
+number_setups = st.number_input('Enter Number of Setups per pool', 1)
 
 total_time = (efficiency_factor*average_time*(number_players*number_players-1))/(2*number_setups)
 
